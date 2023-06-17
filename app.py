d677003784b4258d4ea1bb10a429609322cb8705
deftydev <devanshgupta79212346@gmail.com>
2023-06-17 12:14:08 +0530

    Flask app implemented

--- app.py
+++ app.py
@@ -9,35 +9,32 @@ app=application
 
 
 
-# # @app.route('/')
-# # def home_page():
-# #     return render_template('index.html')
+@app.route('/')
+def home_page():
+    return render_template('index.html')
 
-# # @app.route('/predict',methods=['GET','POST'])
+@app.route('/predict',methods=['GET','POST'])
 
-# # def predict_datapoint():
-# #     if request.method=='GET':
-# #         return render_template('form.html')
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('form.html')
     
-# #     else:
-# #         data=CustomData(
-# #             carat=float(request.form.get('carat')),
-# #             depth = float(request.form.get('depth')),
-# #             table = float(request.form.get('table')),
-# #             x = float(request.form.get('x')),
-# #             y = float(request.form.get('y')),
-# #             z = float(request.form.get('z')),
-# #             cut = request.form.get('cut'),
-# #             color= request.form.get('color'),
-# #             clarity = request.form.get('clarity')
-# #         )
-# #         final_new_data=data.get_data_as_dataframe()
-# #         predict_pipeline=PredictPipeline()
-# #         pred=predict_pipeline.predict(final_new_data)
+    else:
+        data=CustomData(
+            age=float(request.form.get('age')),
+            bmi = float(request.form.get('bmi')),
+            children = float(request.form.get('children')),
+            sex = request.form.get('sex'),
+            smoker= request.form.get('smoker'),
+            region = request.form.get('region')
+        )
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictPipeline()
+        pred=predict_pipeline.predict(final_new_data)
 
-# #         results=round(pred[0],2)
+        results=round(pred[0],2)
 
-#         return render_template('result.html',final_result=results)
+        return render_template('result.html',final_result=results)
 
 
 
